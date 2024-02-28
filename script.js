3658923db137eaca22c74c8094ead70641c1c0d8
roxiii06 <161632794+roxiii06@users.noreply.github.com>
2024-02-28 19:42:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openArticles () {
+    window.location.href = 'index.html';  
+    
+ }
+ 
+ function openContact () {
+    window.location.href = 'Contact.html';  
+    
+ }
+ 
+ 
+ function openReviews () {
+    window.location.href = 'Reviews.html';  
+    
+ }
